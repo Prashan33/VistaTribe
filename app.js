fe3c2bb8b88de5ffa2a67b924d90d2fa8b959bc1
Prashan Adhikari <prashanadhikari@Mac.lan>
2025-08-02 01:27:25 -0500

    Fix error handler and clean up session store

--- app.js
+++ app.js
@@ -11,94 +11,89 @@ const ExpressError = require("./utils/ExpressError");
 const wrapAsync = require("./utils/wrapAsync");
 const { listingSchema, reviewSchema } = require("./schema.js");
 const session = require("express-session");
-const MongoStore =  require('connect-mongo');
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-
- 
-const User = require("./models/user"); 
-
+const User = require("./models/user");
 
 // Import routes
 const listingRoutes = require("./routes/listing");
 const reviewRoutes = require("./routes/reviews");
 const userRoutes = require("./routes/user");
 const apiRoutes = require("./routes/api");
+
 const app = express();
 
-
-
+// MongoDB connection
 const dbUrl = process.env.ATLASDB_URL;
+
 mongoose.connect(dbUrl)
   .then(() => console.log("Connected to MongoDB"))
   .catch(err => console.error("MongoDB connection error:", err));
 
+// View engine setup
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
+
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", apiRoutes);
 
+// Session store
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
-    secret: process.env.SECRET, 
+    secret: process.env.SECRET,
   },
-  touchAfter: 24 * 3600 ,
+  touchAfter: 24 * 3600,
 });
-store.on("error", function (e) {
-  console.log("SESSION STORE ERROR ", e);
-});
-
-const sessionOptions = {
-  store,
-  secret:  process.env.SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie:{
-    expires: Date.now() + 7 * 24 * 60 * 60 *1000,
-    maxAge: 7 * 24 * 60 * 60 *1000,
-    httpOnly: true,
- 
-  },
-};
-
-
 
 store.on("error", function (e) {
   console.log("SESSION STORE ERROR ❌", e);
 });
 
+const sessionOptions = {
+  store,
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  },
+};
+
 app.use(session(sessionOptions));
 app.use(flash());
 
+// Passport config
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
+// Flash and user middleware
 app.use((req, res, next) => {
-  res.locals.currentUser = req.user;        
-  res.locals.success = req.flash("success");  
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
 
-// Mount Routes
+// Mount routes
 app.use("/listings", listingRoutes);
 app.use("/listings/:id/reviews", reviewRoutes);
 app.use("/", userRoutes);
 
-
-
-// 404 route
+// 404 handler
 app.use((req, res, next) => {
-  next(new ExpressError(404, "Page Not Found"));
+  next(new ExpressError("Page Not Found", 404)); // ✅ fixed argument order
 });
 
 // Global error handler
@@ -115,8 +110,7 @@ app.use((err, req, res, next) => {
   `);
 });
 
-
-
+// Start server
 app.listen(8080, () => {
   console.log("🚀 Server running at http://localhost:8080");
 });
